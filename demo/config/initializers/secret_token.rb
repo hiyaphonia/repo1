2763a3de1963d31eff2aaa2efc553583dc366578
pavi <pavitir121@gmail.com>
2012-11-08 11:48:47 -0600

    "deleted files"

--- demo/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Demo::Application.config.secret_token = 'f792acbf701792e3d7e707d802a530ccf6a29942d1d83230fbb3deaa1405c64ce69130ded61a5e96d9df738f07e3eb7ab175643a1a37fb41e62c5a90e0a2c205'
